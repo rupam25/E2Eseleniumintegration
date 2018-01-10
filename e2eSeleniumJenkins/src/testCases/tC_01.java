1904956e074f5e191275206c4820c3ee679f8a64
user <user@192.168.0.131>
2018-01-10 17:16:38 +0530

    auomation project first commit

---
+++ e2eSeleniumJenkins/src/testCases/tC_01.java
@@ -0,0 +1,25 @@
+package testCases;
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class tC_01 {
+
+@Test
+public void testcase1()throws InterruptedException  {
+	System.setProperty("webdriver.chrome.driver", "E:\\PracticeWorkspace\\e2eSeleniumJenkins\\driver\\chromedriver.exe");
+
+	ChromeDriver driver= new ChromeDriver();
+	driver.get("http://www.faceook.com");
+	driver.manage().window().maximize();
+	Thread.sleep(5000);
+	driver.findElement(By.id("email")).sendKeys("rupam_life@yahoo.co.in");
+	Thread.sleep(2000);
+	driver.findElement(By.id("pass")).sendKeys("asl@1234");
+	//Thread.sleep(5000);
+	//driver.findElement(By.xpath("//*[@type=\"submit\"]")).click();
+	//Thread.sleep(2000);
+}
+
+
+
+}
